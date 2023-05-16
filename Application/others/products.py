0c1b2038e2b121aa2a4b8804c99892d1e182b78e
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-05-16 20:43:00 +0630

    update

--- Application/others/products.py
+++ Application/others/products.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from Application.models import Category, Product
 
 class Products:
     def home(self, request):
-        return render(request, 'others/products.html')
+        all_products = Product.objects.all().order_by('-id')
+
+        p = Paginator(all_products, 12)
+        page = request.GET.get("page")
+
+        if all_products is not None:
+            items = p.get_page(page)
+        else:
+            items = None
+
+        context = {
+            "items": items
+        }
+        return render(request, 'others/products.html',
+                        context)
         
