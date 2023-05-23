00338907c05c2c979621ef00dbb39273e73f7e10
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-05-23 23:43:19 +0630

    update

--- app_controller/Order/order/order_complete.py
+++ app_controller/Order/order/order_complete.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-
+from app_controller.Order.models import Cart
 
 class Order:
     def success(self, request):
-        return render(request, 'order/order_success.html')
+        if request.user.is_authenticated:
+            cart = Cart.objects.filter(user=request.user)
+            cart_len = len(list(cart))
+        else:
+            cart_len = 0
+
+        context = {
+            "cart_noti": cart_len
+        }
+
+        return render(request, 'order/order_success.html',
+                    context)
 
     def fail(self, request):
-        return render(request, 'order/order_fail.html')
+        if request.user.is_authenticated:
+            cart = Cart.objects.filter(user=request.user)
+            cart_len = len(list(cart))
+        else:
+            cart_len = 0
+
+        context = {
+            "cart_noti": cart_len
+        }
+        
+        return render(request, 'order/order_fail.html',
+                    context)
         
