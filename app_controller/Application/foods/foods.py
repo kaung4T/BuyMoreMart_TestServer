b91acaff0b141e6ea7e5e96ffc831514bedcc14c
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-05-26 12:48:10 +0630

    update

--- app_controller/Application/foods/foods.py
+++ app_controller/Application/foods/foods.py
@@ -45,17 +45,49 @@ class Food:
 
 
     def food_type(self, request, name):
-        product = Product.objects.filter(product_type=name)
+        if request.user.is_authenticated:
+            cart = Cart.objects.filter(user=request.user)
+            cart_len = len(list(cart))
+        else:
+            cart_len = 0
 
+        # brand group
+        if Category.objects.filter(id=1).exists():
+            foods_id = Category.objects.get(id=1)
+            foods = Product.objects.filter(category=foods_id.id).order_by('-id')
+        else:
+            foods = None
+            
+        
+        if Product_type.objects.filter(name=name).exists():
+            pt = Product_type.objects.get(name=name)
+            product = Product.objects.filter(product_type=pt.id)
+        else:
+            product = None
         
         p = Paginator(product, 12)
         
         page = request.GET.get("page")
-        items = p.get_page(page)
+
+        if product is not None:
+            items = p.get_page(page)
+        else:
+            items = None
+
+
+        # Category group
+        if Category.objects.filter(id=1).exists():
+            categrory_id = Category.objects.get(id=1)
+            item_type = Product_type.objects.filter(category=categrory_id.id)
+        else:
+            item_type = None
 
         context = {
             "items": items,
-            "name": name
+            "foods": foods,
+            "name": name,
+            "cart_noti": cart_len,
+            "item_type": item_type
         }
         return render(request, 'foods/foods.html',
                         context)
