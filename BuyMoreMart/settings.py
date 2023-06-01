14b38e194533871a851aa11b5f46a0c08195d09f
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-06-01 10:18:16 +0630

    update

--- BuyMoreMart/settings.py
+++ BuyMoreMart/settings.py
@@ -109,6 +109,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('db_name'),
+#         'USER': config('db_user'),
+#         'PASSWORD': config('db_password'),
+#         'HOST': config('db_host'),
+#         'PORT': '',
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
