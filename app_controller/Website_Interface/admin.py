8762a2eb6dcea14e46506db46c73111abcf5472e
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-06-01 20:21:24 +0630

    update

--- app_controller/Website_Interface/admin.py
+++ app_controller/Website_Interface/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-from app_controller.Website_Interface.models import Index_ImageGroup
+from app_controller.Website_Interface.models import Info, Header_ImageGroup, Index_ImageGroup
 # Register your models here.
 
+
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ['phone',
+                    'email',
+                    'address',
+                    'location']
+
+class Header_ImageGroupAdmin(admin.ModelAdmin):
+    list_display = ['one_image1',
+                    'one_image2',
+                    'two_image1',
+                    'two_image2',
+                    'three_image1',
+                    'three_image2',
+                    'four_image1',
+                    'four_image2',
+                    'five_image1',
+                    'five_image2']
 
 
 class Index_ImageGroupAdmin(admin.ModelAdmin):
@@ -19,5 +37,6 @@ class Index_ImageGroupAdmin(admin.ModelAdmin):
                     'four_image3']
 
 
+admin.site.register(Info, InfoAdmin)
+admin.site.register(Header_ImageGroup, Header_ImageGroupAdmin)
 admin.site.register(Index_ImageGroup, Index_ImageGroupAdmin)
-
--- app_controller/Website_Interface/models.py
+++ app_controller/Website_Interface/models.py
@@ -2,6 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+
+class Info(models.Model):
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    location = models.URLField(null=True, blank=True)
+
+
+class Header_ImageGroup(models.Model):
+    one_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    one_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+
+    two_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    two_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    
+    three_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    three_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+
+    four_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    four_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+
+    five_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+    five_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
+
+
 class Index_ImageGroup(models.Model):
     one_image1 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
     one_image2 = models.ImageField(upload_to='website_interface', null=True, blank=True, default=None)
